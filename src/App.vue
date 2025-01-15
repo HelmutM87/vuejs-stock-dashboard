313a6c504d429062b392264bad59bad11bd29ee6
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2025-01-15 17:27:14 -0300

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
- <template>
-  <div id="app">
-    <!-- Inhalte hier hinzufügen -->
-  </div>
-</template>
+  <!-- <div id="app"> -->
+    <!-- Hier wird die TopCard-Komponente verwendet -->
+    <TopCard>
+      <h1>Top Card Title</h1>
+      <p>Some content inside the card.</p>
+    </TopCard>
+  <!-- </div> -->
 </template>
 
 <script>
+import TopCard from './components/TopCard.vue';
 
 export default {
   name: 'App',
   components: {
-    
-  }
-}
+    TopCard,
+  },
+};
 </script>
 
 <style>
 body {
-  margin: 0
+  margin: 0;
 }
 #app {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
-width: 100vw;
-min-height: 100vh;
+  width: 100vw;
+  min-height: 100vh;
+  padding:100px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/TopCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TopCard',
+  };
+  </script>
+  
+  <style scoped>
+  .card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 20px 24px;
+  }
+  </style>
+  
